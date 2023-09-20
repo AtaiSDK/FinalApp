394aa07b6e390d38e4f9ea5b4721f5ccd4694c92
User <https://github.com/AtaiSDK/HomeWork.git>
2023-09-20 23:54:06 +0300

    H/W1

---
+++ app/src/main/java/com/example/finalapp/adapters/FragmentPageAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.finalapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.finalapp.cameras.CamerasFragment
+import com.example.finalapp.doors.DoorsFragment
+
+class FragmentPageAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return if (position == 0)
+            CamerasFragment()
+        else
+            DoorsFragment()
+    }
+}
---
+++ app/src/main/java/com/example/finalapp/cameras/model.kt
@@ -0,0 +1,6 @@
+package com.example.finalapp.cameras
+
+data class model(
+    val title: String,
+    val image: String
+)
